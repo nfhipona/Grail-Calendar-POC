0fd37c076e98ad4b248658eab0ff7c9511164039
Neil Francis Hipona <neil.francis.hipona@codeandtheory.com>
2022-06-08 15:01:15 +0800

    Initial Commit

---
+++ grail-calendar-poc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  grail-calendar-poc
+//
+//  Created by Neil Francis Hipona on 6/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ grail-calendar-poc/grail_calendar_pocApp.swift
@@ -0,0 +1,17 @@
+//
+//  grail_calendar_pocApp.swift
+//  grail-calendar-poc
+//
+//  Created by Neil Francis Hipona on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct grail_calendar_pocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
